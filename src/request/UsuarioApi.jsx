51e88db6d1aa47700c94a0a6f1f4195127e6e642
Lucas Gabriel <lucasgsalmeida21@gmail.com>
2024-12-11 11:07:24 -0300

    Primeiro

--- src/request/UsuarioApi.jsx
+++ src/request/UsuarioApi.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_URL = "https://apigestao.lumendigital.com.br";
+const API_URL = process.env.REACT_APP_API_URL || 'http://localhost:8080';
 
 const getAuthHeader = () => {
   const tokenData = JSON.parse(localStorage.getItem("token"));
